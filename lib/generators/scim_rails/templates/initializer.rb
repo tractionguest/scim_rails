66f62f686fc2cc8339acfe56f0773be880f59988
Matthew Yang <mwyang99@gmail.com>
2020-05-29 13:32:46 -0700

    add canonical_reference to initializer

--- lib/generators/scim_rails/templates/initializer.rb
+++ lib/generators/scim_rails/templates/initializer.rb
@@ -56,6 +56,10 @@ ScimRails.configure do |config|
   # Method called on user model to reprovision a user.
   config.user_reprovision_method = :unarchive!
 
+  # Unique identifier for model instances.  SHOULD be set
+  # to :id or :uuid
+  config.canonical_reference = :id
+
   # Hash of queryable attribtues on the user model. If
   # the attribute is not listed in this hash it cannot
   # be queried by this Gem. The structure of this hash
