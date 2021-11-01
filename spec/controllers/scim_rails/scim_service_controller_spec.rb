51a473b1c25b3fe260523bf553e2048dbc031e9f
Andrew Burns <andrew@fetlife.com>
2021-11-01 10:46:56 -0600

    wip - scim_service_controller_spec - 0 failing

--- spec/controllers/scim_rails/scim_service_controller_spec.rb
+++ spec/controllers/scim_rails/scim_service_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe ScimRails::ScimServiceController, type: :controller do
       it "returns scim+json content type" do
         get :configuration
 
-        expect(response.content_type).to eq("application/scim+json")
+        expect(response.content_type).to eq("application/scim+json; charset=utf-8")
       end
 
       it "fails with no credentials" do
@@ -43,7 +43,7 @@ RSpec.describe ScimRails::ScimServiceController, type: :controller do
       it "returns scim+json content type" do
         get :configuration
 
-        expect(response.content_type).to eq("application/scim+json")
+        expect(response.content_type).to eq("application/scim+json; charset=utf-8")
       end
 
       it "is successful with valid credentials" do
